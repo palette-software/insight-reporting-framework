09d0a9c0e99b3727e4093331f3362629288122ff
SELLER Peter Andras <spa@palette-software.com>
2016-09-02 11:22:12 +0200

    Add execute_worflow and preprocess_workflow functions

--- reporting.py
+++ reporting.py
@@ -3,6 +3,26 @@ import psycopg2
 import yaml
 
 
+def execute_worflow(workflow, config):
+    schema_name = config["Schema"]
+    conn = psycopg2.connect(
+        "dbname={Database} user={User} password={Password} host={Host} port={Port}".format(**config))
+    cur = conn.cursor()
+    cur.execute("set search_path = " + schema_name)
+    for i in workflow:
+        cur.execute("select " + i)
+        for record in cur:
+            print(record[0])
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def preprocess_workflow(workflow, config):
+    schema_name = config["Schema"]
+    return [item.replace("#schema_name#", "'" + schema_name + "'") for item in workflow]
+
+
 def main():
     logging.basicConfig(filename='./insight-reporting.log', level=logging.DEBUG)
     logging.info('Start Insight Reporting.')
@@ -13,20 +33,8 @@ def main():
     with open("./workflow.yml") as workflow_file:
         workflow = yaml.load(workflow_file)
 
-    schema_name = config["Schema"]
-
-    conn = psycopg2.connect("dbname={Database} user={User} password={Password} host={Host} port={Port}".format(**config))
-    cur = conn.cursor()
-    cur.execute("set search_path = " + schema_name)
-
-    for i in workflow:
-        cur.execute("select " + i.replace("#schema_name#", "'" + schema_name + "'"))
-        for record in cur:
-            print(record[0])
-
-    conn.commit()
-    cur.close()
-    conn.close()
+    preprocessed_workflow = preprocess_workflow(workflow, config)
+    execute_worflow(preprocessed_workflow, config)
 
     logging.info('End Insight Reporting.')
 
