57e7dcce1d0ef9aea9498e341e852d84f76850af
SELLER Peter Andras <spa@palette-software.com>
2016-09-02 11:09:49 +0200

    Move code to main function. Rename variables.

--- reporting.py
+++ reporting.py
@@ -2,29 +2,34 @@ import logging
 import psycopg2
 import yaml
 
-logging.basicConfig(filename='./insight-reporting.log', level=logging.DEBUG)
-logging.info('Start Insight Reporting.')
 
-with open("./Config.yml") as fd:
-    d = yaml.load(fd.read())
+def main():
+    logging.basicConfig(filename='./insight-reporting.log', level=logging.DEBUG)
+    logging.info('Start Insight Reporting.')
 
-with open("./workflow.yml") as fd:
-    wf = yaml.load(fd.read())
+    with open("./Config.yml") as config_file:
+        config = yaml.load(config_file)
+
+    with open("./workflow.yml") as workflow_file:
+        workflow = yaml.load(workflow_file)
+
+    schema_name = config["Schema"]
+
+    conn = psycopg2.connect("dbname={Database} user={User} password={Password} host={Host} port={Port}".format(**config))
+    cur = conn.cursor()
+    cur.execute("set search_path = " + schema_name)
+
+    for i in workflow:
+        cur.execute("select " + i.replace("#schema_name#", "'" + schema_name + "'"))
+        for record in cur:
+            print(record[0])
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    logging.info('End Insight Reporting.')
 
 
-schema_name = d["Schema"]
-
-conn = psycopg2.connect("dbname={Database} user={User} password={Password} host={Host} port={Port}".format(**d))
-cur = conn.cursor()
-cur.execute("set search_path = " + schema_name)
-
-for i in wf:
-    cur.execute("select " + i.replace("#schema_name#", "'" + schema_name + "'"))
-    for record in cur:
-        print(record[0])
-
-conn.commit()
-cur.close()
-conn.close()
-
-logging.info('End Insight Reporting.')
+if __name__ == '__main__':
+    main()
