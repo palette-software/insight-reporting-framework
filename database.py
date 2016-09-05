ab2c65097999409ef117d09f9222a1e175b141de
SELLER Peter Andras <spa@palette-software.com>
2016-09-05 11:38:12 +0200

    Use new (name, query key) format in workflow.yml. Update database to support new format.

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
+import logging
 import psycopg2
 
 
 class Database(object):
     def __init__(self, kwargs):
+        logging.debug("Start database connection")
         self.connection = psycopg2.connect(
             "dbname={Database} user={User} password={Password} host={Host} port={Port}".format(**kwargs))
+        logging.debug("End database connection")
         self.schema_name = kwargs['Schema']
 
     def transaction(self, item):
@@ -14,7 +17,6 @@ class Database(object):
                     result = self._execute_transaction(cursor, item)
                 else:
                     result = self._execute(cursor, item)
-
         return result
 
     def _execute_transaction(self, cursor, items):
@@ -25,8 +27,9 @@ class Database(object):
         return result
 
     def _execute(self, cursor, item):
-        cursor.execute(item)
-
+        logging.debug('Start "{}"'.format(item['name']))
+        cursor.execute(item['query'])
+        logging.debug('End "{}"'.format(item['name']))
         if cursor.rowcount < 1:
             return []
 
@@ -34,3 +37,5 @@ class Database(object):
 
     def __del__(self):
         self.connection.close()
+        logging.debug("Database connection is closed")
+
