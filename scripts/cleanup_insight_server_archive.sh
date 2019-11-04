e2bba8e008f57986f2015ada829796360f100fef
SELLER Peter Andras <petispaespea@gmail.com>
2019-11-04 11:04:36 +0100

    Fix archive folder cleanup (_temp folder issue) cleanup serverlogs-archives

--- scripts/cleanup_insight_server_archive.sh
+++ scripts/cleanup_insight_server_archive.sh
@@ -3,14 +3,16 @@
 # Clean up the .csv.gz files in the archive directory
 #
 
-INSIGHT_SERVER_DATA_DIR="/data/insight-server/uploads"
+INSIGHT_SERVER_ARCHIVES_DIR="/data/insight-server/uploads/palette/archive"
+HOST_DIRS=$(find ${INSIGHT_SERVER_ARCHIVES_DIR} -mindepth 1 -maxdepth 1 -type d)
+for HOST_DIR in ${HOST_DIRS}; do
+    # In order not to flood logs with deleted csv files the output is suppressed
+    "$(dirname "$0")"/cleanup_dir.sh "${HOST_DIR}" 61 false > /dev/null
+done
 
-CLUSTER_DIRS=$(find ${INSIGHT_SERVER_DATA_DIR} -mindepth 1 -maxdepth 1 -type d)
-
-for CLUSTER_DIR in ${CLUSTER_DIRS}; do
-    ARCHIVE_DIRS=$(find "${CLUSTER_DIR}/archive" -mindepth 1 -maxdepth 1 -type d)
-    for ARCHIVE_DIR in ${ARCHIVE_DIRS}; do
-        # In order not to flood logs with deleted csv files the output is suppressed
-        "$(dirname "$0")"/cleanup_dir.sh "${ARCHIVE_DIR}" 61 false > /dev/null
-    done
+INSIGHT_SERVERLOGS_ARCHIVES_DIR="/data/insight-server/serverlogs-archives/palette/uploads/public"
+HOST_DIRS=$(find "${INSIGHT_SERVERLOGS_ARCHIVES_DIR}" -mindepth 1 -maxdepth 1 -type d)
+for HOST_DIR in ${HOST_DIRS}; do
+    # In order not to flood logs with deleted csv files the output is suppressed
+    "$(dirname "$0")"/cleanup_dir.sh "${HOST_DIR}" 15 false > /dev/null
 done
